8342c7affdd8b82a7df84f362314b2e0e57a6ef3
Artem Rylov <tema2707@icloud.com>
2022-02-27 21:42:39 +0700

    add menu items

--- src/apps/food_projects/admin.py
+++ src/apps/food_projects/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FoodProject
+from django.db.models import Q
+from .models import FoodProject, MenuItem
 
 
 @admin.register(FoodProject)
@@ -16,3 +17,42 @@ class AdminFoodProject(admin.ModelAdmin):
         if request.user.is_superuser:
             return queryset
         return queryset.filter(owner=request.user)
+
+
+def _get_only_current_user_menu_items(request, queryset):
+    user_projects = FoodProject.objects.filter(owner=request.user)
+
+    any_user_project_filter_condition = Q()
+    for user_project in user_projects:
+        any_user_project_filter_condition = any_user_project_filter_condition | Q(project=user_project)
+    return queryset.filter(any_user_project_filter_condition)
+
+
+@admin.register(MenuItem)
+class AdminMenuItem(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return queryset
+
+        return _get_only_current_user_menu_items(request, queryset)
+
+        # user_projects = FoodProject.objects.filter(owner=request.user)
+        #
+        # any_user_project_filter_condition = Q()
+        # for user_project in user_projects:
+        #     any_user_project_filter_condition = any_user_project_filter_condition | Q(project=user_project)
+        # return queryset.filter(any_user_project_filter_condition)
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        project_selector = context['adminform'].form.fields['project']
+
+        # select first project as default
+        project_selector.initial = 1
+        # filter project by current user
+        project_selector.queryset = FoodProject.objects.filter(owner=request.user)
+
+        return super().render_change_form(request, context, add=add, change=change, form_url=form_url, obj=obj)
--- src/apps/food_projects/models.py
+++ src/apps/food_projects/models.py
@@ -7,3 +7,12 @@ class FoodProject(UUIDModel):
     name = models.CharField(max_length=120)
     description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(UUIDModel):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    project = models.ForeignKey(FoodProject, on_delete=models.CASCADE)
